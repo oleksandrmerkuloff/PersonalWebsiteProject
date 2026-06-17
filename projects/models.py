7737ad180b8082d5ea85e9b46de053826ecdfd2e
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2026-06-17 23:15:36 +0300

    added project app

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+import uuid
+
+from django.db import models
+
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
