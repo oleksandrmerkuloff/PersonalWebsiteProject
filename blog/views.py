ea1fc5fe504c38908eaa7fac1fc3035866e7c9db
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2026-06-25 11:59:54 +0300

    changed design for blog and sinle-postr pages, added new queries to blog view

--- blog/views.py
+++ blog/views.py
@@ -8,18 +8,31 @@ def index(request):
 
 
 def blog_view(request):
-    searching = request.GET.get("q")
-    if searching:
+    searching = request.GET.get("q", "").strip()
+    tag = request.GET.get("tag", "").strip()
+    
+    if searching and tag:
+        posts = Post.objects.prefetch_related("tags").filter(tags__name=tag, title__icontains=searching)
+    elif searching:
         posts = Post.objects.prefetch_related("tags").filter(title__icontains=searching)
+    elif tag:
+        posts = Post.objects.prefetch_related("tags").filter(tags__name=tag)
     else:
         posts = Post.objects.prefetch_related("tags").all()
-    return render(request, "blog.html", {"posts": posts}) # change template name
+        
+    context = {
+        "posts": posts,
+        "search_query": searching,
+        "current_tag": tag,
+    }
+    
+    return render(request, "blog.html", context)
 
 
 def post_view(request, slug):
     queryset = Post.objects.prefetch_related("tags", "chapters")
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, 'single-post.html', {"post": post}) # change template name
+    return render(request, 'single-post.html', {"post": post})
 
 
 def contacts_view(request):
